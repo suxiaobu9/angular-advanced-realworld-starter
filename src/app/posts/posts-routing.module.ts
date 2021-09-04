2f084ae4b9bbbf0c249a4957bd08cb3f640e6488
ari <ari@miniasp.com>
2021-09-04 09:57:39 +0800

    任務01 - 建立路由

---
+++ src/app/posts/posts-routing.module.ts
@@ -0,0 +1,31 @@
+import { CreateComponent } from './create/create.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts.component';
+import { PostComponent } from './post/post.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'post/:id',
+    component: PostComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PostsRoutingModule { }
