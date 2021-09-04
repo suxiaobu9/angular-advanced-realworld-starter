5824ef5e0ed87c65a5627420099a575229a1bb5c
ari <ari@miniasp.com>
2021-09-04 13:41:39 +0800

    04

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
+import { LoginService } from './../login.service';
+import { UserLoginInfo } from './../interfaces/login-info';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: UserLoginInfo = {
+    email: '',
+    password: '',
+  }
+
+  constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    this.loginService.login(this.user).pipe(
+      catchError((error: HttpErrorResponse) => {
+        alert(error.message);
+        return throwError(error);
+      })
+    ).subscribe(result => {
+      localStorage.setItem('token', result.user.token);
+      this.router.navigateByUrl('/');
+    })
   }
 
 }
