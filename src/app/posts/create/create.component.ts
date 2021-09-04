53fc63b786099df9e0a903a079e6e9e2712ea3c5
ari <ari@miniasp.com>
2021-09-04 15:10:27 +0800

    06

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   templateUrl: './create.component.html',
@@ -6,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  post = this.formBuilder.group({
+    title: this.formBuilder.control(''),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.formBuilder.array([
+      this.formBuilder.control('Angular'),
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+    ])
+  })
+
+  get tags(): FormArray {
+    return this.post.get('tags') as FormArray;
+  }
+
+  addTag(tag: string): void {
+    if (!tag)
+      return;
+    this.tags.push(this.formBuilder.control(tag));
+  }
+
+  removeTag(index: number): void {
+    this.tags.removeAt(index);
+  }
+
+  createPost(): void {
+    console.log(this.post.value);
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
